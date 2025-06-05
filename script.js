9e9cb46f840cecef56bb0778523ad509e22a9455
Sudarsanbadrinath <sudarsanbadrinath@gmail.com>
2025-06-05 20:49:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,12 +4,14 @@ document.getElementById('rsvp-form').addEventListener('submit', function(event) 
     let guestName = document.getElementById('guest-name').value;
     let location = document.getElementById('location').value;
     let arrivingByCar = document.getElementById('arriving-by-car').value;
+    let side = document.getElementById('side').value;  // Capture the side (Groom's or Bride's side)
 
     // Create a FormData object and append the form data
     var formData = new FormData();
     formData.append('name', guestName);
     formData.append('location', location);
     formData.append('arriving-by-car', arrivingByCar);
+    formData.append('side', side);  // Append the "side" value here
 
     // Log the FormData to verify it's correct (for debugging)
     for (var pair of formData.entries()) {
@@ -17,7 +19,7 @@ document.getElementById('rsvp-form').addEventListener('submit', function(event) 
     }
 
     // Send the data to Google Apps Script Web App URL
-    fetch('https://script.google.com/macros/s/AKfycbxpT7Z4kToUG4g8MkN_1k93EQIbf1nnzaUMu8diXcxdVsG3mK3xI4wtGQGCE4SYKpSfZg/exec', {
+    fetch('https://script.google.com/macros/s/AKfycbzu4w9Qb-912XIS9U279YHIHacAam4Rl53nr3x-gKjrklY5XR7K2Y3pSi_eisSY9SJF5g/exec', {
         method: 'POST',
         body: formData
     })
